f3679492da2ce2297ede2365fc5251127ccd8bb1
racla <andreluizreis@gmail.com>
2016-10-30 20:00:32 -0400

    Fixed the problem with columns gap.

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -3,5 +3,16 @@
 
     $('.button-collapse').sideNav();
 
+      var currentColumn = 0;
+
+    function countColumn(){
+        if(currentColumn == 3){
+            currentColumn = 0;
+        } else {
+            currentColumn += 1;
+        }
+        return currentColumn;
+    }
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
